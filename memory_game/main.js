f548d8598f60f2f906a893e457f62e77969fffed
Sara Daghustani <sara.daghustani@gmail.com>
2019-05-28 23:10:30 +0300

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,27 +1,24 @@
 
-var cards = ["queen", "queen", "king","king"];
-var cardsInPlay= [];
-var cardOne = cards[0];
+var cards = ["queen", "queen", "king", "king"];
 
-cardsInPlay.push ('cardOne');
-console.log("User flipped " + cardOne);
-
-var cardTwo= cards[2];
-cardsInPlay.push ('cardTwo');
-console.log("User flipped " + cardTwo);
-var cardThree= cards[2];
-
-var cardFour= cards[3]; 
-
-
-    if (cardsInPlay.length ===2) {
+var cardsInPlay = [];
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
     }
+};
 
-if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-}
-    else {
-        alert("Sorry, try again.");
-    }
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    if(cardsInPlay.length === 2) {
+        checkForMatch()
+        }
+    console.log("user flipped" + " " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+};
 
+flipCard(0);
+flipCard(2);
 
